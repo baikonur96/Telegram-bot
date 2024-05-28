adc89597edc10ded4d06e9b5599f5e0ae7d6e826
Adminsvu <Baikonur96@yandex.ru>
2024-05-28 16:39:05 +0300

    ver.1

--- src/main/java/com/example/telegram_bot/TelegramBot.java
+++ src/main/java/com/example/telegram_bot/TelegramBot.java
@@ -1,7 +1,9 @@
 package com.example.telegram_bot;
 
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class TelegramBot extends TelegramLongPollingBot {
@@ -10,13 +12,19 @@ public class TelegramBot extends TelegramLongPollingBot {
         super(options, botToken);
     }
 
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-
+        if (update.hasMessage() && update.getMessage().hasText()){
+            var text = update.getMessage().getText();
+            var chatId = update.getMessage().getChatId();
+            SendMessage sendMessage = new SendMessage(chatId.toString(), text);
+            sendApiMethod(sendMessage);
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return "My-test-bot";
     }
 }
---
+++ src/main/java/com/example/telegram_bot/openai/OpenAIClient.java
@@ -0,0 +1,13 @@
+package com.example.telegram_bot.openai;
+
+import org.springframework.web.client.RestClient;
+
+public class OpenAIClient {
+    private final String token;
+
+    private final RestClient restClien;
+
+    public ChatCompletionObject createChatCompletion
+
+
+}
